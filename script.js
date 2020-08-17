436001a880a298a50c004931e010b99e5dc74d25
Therealajahn <therealajahn@gmail.com>
2020-08-17 18:26:12 -0500

    put buttons on top and bottom

--- script.js
+++ script.js
@@ -2,25 +2,35 @@ document.cookie = "promo_shown=1; Max-Age=2600000; SameSite=None; Secure"
 console.log('document.cookie:', document.cookie)
 
 
-let startButton = document.getElementsByClassName('leaves-button')[0];
+let startButton1 = document.getElementsByClassName('begin')[0];
+let startButton2 = document.getElementsByClassName('begin')[1];
 let leaves = document.getElementsByClassName('leaves')[0];
 let waste = document.getElementsByClassName('waste-days')[0];
 let toggle = false;
 
-startButton.addEventListener('click', ()=>{
-    
-    console.log('yes')
+
+function controlAudioVisual(){
+   
     if(!toggle){
         leaves.play()
         waste.play()
+        startButton1.innerHTML = 'Stop Things(click this)';
+        startButton2.innerHTML = 'Stop Things(click this)';
     }else{
         leaves.pause();
         waste.pause();
+        startButton1.innerHTML = 'Begin Things(click this)';
+        startButton2.innerHTML = 'Begin Things(click this)';
     }
     toggle = !toggle;
-    console.log('toggle', toggle)
+}
 
+startButton1.addEventListener('click', ()=>{
+    controlAudioVisual();
 })
 
+startButton2.addEventListener('click', ()=>{
+    controlAudioVisual();
+})
     
 
